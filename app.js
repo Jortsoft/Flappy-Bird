c7c795d971cb5da360cd6e5b9a09a721fd76d182
Jortsoft <48485731+Jortsoft@users.noreply.github.com>
2021-10-19 20:35:01 +0400

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const config = {
     width: 480,
     height: 640,
     scene: [ play ],
+    scale: {
+        mode: Phaser.Scale.FIT,
+    },
     physics:{
         default:'arcade',
         arcade:{debug:false}
